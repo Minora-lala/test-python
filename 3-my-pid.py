9f0e97e29aadd3502fa3bff362155922da2f8fb8
PROSPERO Romain <38522682+prospero-romain@users.noreply.github.com>
2021-02-15 15:25:59 +0100

    Update 3-my-pid.py

--- 3-my-pid.py
+++ 3-my-pid.py
@@ -1,21 +1,26 @@
-import os
-
-def child():
-   print('A new child ',  os.getpid())
-   os._exit(0)  
-
-def parent():
-   while True:
-      newpid = os.fork()
-      if newpid == 0:
-         child()
-      else:
-         pids = (os.getpid(), newpid)
-         print("parent: %d, child: %d\n" % pids)
-      reply = input("q for quit / c for new fork")
-      if reply == 'c': 
-          continue
-      else:
-          break
-
-parent()
+# importing os module  
+import os 
+  
+  
+# Create a child process 
+# using os.fork() method  
+pid = os.fork() 
+  
+# pid greater than 0 represents 
+# the parent process  
+if pid > 0 : 
+    print("I am parent process:") 
+    print("Process ID:", os.getpid()) 
+    print("Child's process ID:", pid) 
+  
+# pid equal to 0 represnts 
+# the created child process 
+else : 
+    print("\nI am child process:") 
+    print("Process ID:", os.getpid()) 
+    print("Parent's process ID:", os.getppid()) 
+  
+  
+# If any error occured while 
+# using os.fork() method 
+# OSError will be raised 
